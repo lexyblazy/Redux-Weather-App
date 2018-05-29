df780abf73b14df77443cb4e6a4450c4883a5caa
lexy <lexyblazy@gmail.com>
2018-05-29 12:28:14 +0100

    changed api to https

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const FETCH_WEATHER = "fetch_weather";
 
 const API_KEY = "&appid=b8cb883613842c36ce2118a24097f158";
-const rootUrl = `http://api.openweathermap.org/data/2.5/forecast`;
+const rootUrl = `https://api.openweathermap.org/data/2.5/forecast`;
 
 export function fetchWeather(city) {
   const url = `${rootUrl}?q=${city}${API_KEY}`;
