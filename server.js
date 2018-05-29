61673e7243b2c81ebac5b10d01109529dddd320a
lexy <lexyblazy@gmail.com>
2018-05-29 12:32:50 +0100

    enable cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.static(`${__dirname}/build`));
 app.get("*", (req, res) => {
